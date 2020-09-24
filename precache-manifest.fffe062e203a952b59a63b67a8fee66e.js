2760009cf0c9b5848d7b9b371b3839fcef180f60
unknown <florin.astefanoaie17@gmail.com>
2020-09-24 17:12:18 +0300

    Updates

---
+++ precache-manifest.fffe062e203a952b59a63b67a8fee66e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09759ab2b8dedea17634975ede0123b",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "0bcf51e131406d731988",
+    "url": "/Robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9e03ffd60e736c806971",
+    "url": "/Robots/static/css/main.1f35e81c.chunk.css"
+  },
+  {
+    "revision": "0bcf51e131406d731988",
+    "url": "/Robots/static/js/2.ceb11a39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robots/static/js/2.ceb11a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e03ffd60e736c806971",
+    "url": "/Robots/static/js/main.0c3427b5.chunk.js"
+  },
+  {
+    "revision": "3a5a6c71276ae8d83edb",
+    "url": "/Robots/static/js/runtime-main.4ac3e437.js"
+  },
+  {
+    "revision": "57c6004e7000050ba4ce91a2eb6a27fe",
+    "url": "/Robots/static/media/Platonica-laZZ.57c6004e.woff"
+  }
+]);
